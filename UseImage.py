fb2601124ae63a41ce72ba19732b0c78c9c63933
merlosus <merlosus@yahoo.com>
2019-04-19 17:49:00 +1000

    Update UseImage.py

--- UseImage.py
+++ UseImage.py
@@ -4,6 +4,4 @@ class PostImage:
             self.content = content
 
     def set_token(self):
-        setattr(self, 'key', ('EAAIO05sRYDgBAHOrwVWMyf7gdchZB8oL0MAzQNWZCMgofUexJ56l26WM203ZC4wU6Sk'
-                              'J90sTBfE4mQu3Nmb7UKwZCOqA6RhYruc4gJZBQRSzHZCoPHrzRZB3sxcZB4F2ma2ZBZC'
-                              'T7ZC1FMMAuEWuqv9SKeSb4CPKNyH97diUi3SwYkfwi1d2tJZAbZBo3'))
+        setattr(self, 'key', ('######'))
